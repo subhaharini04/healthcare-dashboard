1ffdb6e3467e5c81d328628f162267eb615afc03
subhaharini04 <subhaharini.act@gmail.com>
2025-05-26 13:29:26 +0530

    combine header and siderbar and aslo created  alyotu dashboardmain content

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
 import { Sidebar } from './layout/Sidebar'
 import { Header } from './layout/Header'
+import { DashboardMainContent } from './layout/DashboardMainContent'
 
 function App() {
 
   return (
     <>
-      <Header/>
+      <DashboardMainContent/>
     </>
   )
 }
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -6,14 +6,14 @@ import profile from "../assets/profile.jpg";
 
 export const Header = () => {
     return (
-        <div className=''>
-            <div className='flex justify-between items-center'>
-                <div className='flex items-center space-x-2'>
-                    <div className='relative'>
+        <div className='pl-10 pt-10'>
+            <div className='flex justify-between items-center '>
+                <div className='flex items-center gap-4 flex-grow'>
+                    <div className='w-full max-w-xl relative '>
                         <FiSearch className='absolute top-2 left-2 text-2xl text-blue-900' />
                         <input type="text"
                             placeholder='Search'
-                            className='border-2 border-gray-100 rounded-md w-74 max-w-md h-10 pl-10' />
+                            className='border-2 border-gray-100 rounded-md w-full max-w-xl h-10 pl-10' />
                     </div>
                     <div className='border-2 border-gray-100 rounded-md h-10 w-10  flex items-center justify-center pt-1'>
                         <IoNotifications className='text-2xl text-blue-900' />
@@ -22,7 +22,7 @@ export const Header = () => {
                 <div className='flex items-center space-x-2 mr-10'>
                     <img src={profile} alt="profile"
                         className='w-10 h-10 rounded-md' />
-                    <div className='bg-blue-900 w-10 h-10 rounded-md text-white  flex items-center justify-center pl-1 pt-1 cursor-pointer'>
+                    <div className='bg-blue-900 w-10 h-10 rounded-md text-white  flex items-center justify-center  pt-1 cursor-pointer'>
                         <IoAddOutline className='text-white text-3xl'/>
                     </div>
                 </div>
